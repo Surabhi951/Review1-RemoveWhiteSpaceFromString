cb74d52c772a12e69e5622ad1620524170512a92
Surabhi951 <surabhibhagat95@gmail.com>
2022-10-26 09:39:46 +0530

    modified: Removed white spaces from string program

--- src/com/bridgelabz/RemoveSpaceFromString.java
+++ src/com/bridgelabz/RemoveSpaceFromString.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class RemoveSpaceFromString {
 
     public static void main(String[] args) {
-        RemoveSpaceFromString removeSpaceFromString = new RemoveSpaceFromString();
-        removeSpaceFromString.removeWhiteSpacesFromString();
-    }
-
-    public void removeWhiteSpacesFromString(){
         System.out.println("Enter String: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        RemoveSpaceFromString removeSpaceFromString = new RemoveSpaceFromString();
+        removeSpaceFromString.removeWhiteSpacesFromString(str);
+    }
+
+    public void removeWhiteSpacesFromString(String str){
         str = str.replaceAll("\\s", "");
         System.out.println("After removing white spaces from string is: " + str);
     }
